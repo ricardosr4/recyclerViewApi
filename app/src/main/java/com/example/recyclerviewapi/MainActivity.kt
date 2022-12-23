fa193e1212ed0087bf7e6f17e64fd4bae7c2078e
Ricardo <ricardosr4@gmail.com>
2022-12-22 21:40:57 -0300

    aqui va un avance del ReciclerviewApi

--- app/src/main/java/com/example/recyclerviewapi/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapi/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.recyclerviewapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recyclerviewapi.databinding.ActivityMainBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var Binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breeds/")
+        .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
